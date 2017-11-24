0233cb07547cbad1209bdf210527fc587f684dd6
RakibHossain <RakibHossain@users.noreply.github.com>
2017-11-25 01:51:04 +0600

    Create archive_details.blade.php
    archive added

---
+++ resources/views/writter/archive_details.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- Content Header (Page header) -->
+<section class="content-header">
+
+    <h1>Archive Details<small></small></h1>
+
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+        <li class="active">archives</li>
+    </ol>
+
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+    
+	
+</section><!-- /.content -->
+
+@endsection
